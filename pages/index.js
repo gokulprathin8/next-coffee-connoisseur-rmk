2c97d6c504bbd0c97f989e605cf7c9e138c28421
Gokul Prathin <gokulprathin8@gmail.com>
2022-05-07 22:02:11 +0530

    added foursqure api for real world data

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,14 @@ import Banner from "../components/banner";
 import Card from "../components/card";
 
 import coffeeStores from "../data/coffee-stores.json";
+import {fetchCoffeeStores} from "../lib/coffee-stores";
 
 export async function getStaticProps(context) {
+
+    const coffeeStores = await fetchCoffeeStores();
     return {
         props: {
-            coffeeStores
+            coffeeStores: coffeeStores.results
         }
     }
 }
@@ -41,8 +44,8 @@ export default function Home(props) {
                   {props.coffeeStores.map(coffeeStore => {
                       return (
                           <Card
-                              key={coffeeStore.id}
-                              imageURL={coffeeStore.imgUrl}
+                              key={coffeeStore.fsq_id}
+                              imageURL={coffeeStore.imgUrl || 'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'}
                               name={coffeeStore.name}
                               href={`/coffee-store/${coffeeStore.id}`}
                               className={styles.card}
